2cb1eb495a34d674a1409f147ad8e855fbdc8226
Ruben Rodrigues <ruben.rodrigues@fenergo.com>
2020-09-20 21:54:50 +0100

    End of section 5

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -5,10 +5,7 @@ namespace Persistence
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-
-        }
+        public DataContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
         public DbSet<Activity> Activities { get; set; }
